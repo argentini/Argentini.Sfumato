5f4e84ea223ae7b439940d454e24552026edb54f
Michael Argentini <michael@argentini.us>
2023-10-08 20:35:49 -0400

    Run scripts use release mode

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-dotnet run --project Argentini.Sfumato/Argentini.Sfumato.csproj --path Argentini.Sfumato.Tests/SampleWebsite --diagnostics
+dotnet run --project Argentini.Sfumato/Argentini.Sfumato.csproj --path Argentini.Sfumato.Tests/SampleWebsite --release --diagnostics
--- watch.sh
+++ watch.sh
@@ -1,1 +1,1 @@
-dotnet run --project Argentini.Sfumato/Argentini.Sfumato.csproj --path Argentini.Sfumato.Tests/SampleWebsite/ --diagnostics --watch
+dotnet run --project Argentini.Sfumato/Argentini.Sfumato.csproj --path Argentini.Sfumato.Tests/SampleWebsite/ --release --diagnostics --watch
