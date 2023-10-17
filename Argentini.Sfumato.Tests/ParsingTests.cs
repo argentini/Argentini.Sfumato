104558e7eb0345d42d4bf7a84b3537d99ffc6539
Michael Argentini <michael@argentini.us>
2023-10-16 22:13:16 -0400

    Added a bunch of helper unit tests

--- Argentini.Sfumato.Tests/ParsingTests.cs
+++ Argentini.Sfumato.Tests/ParsingTests.cs
@@ -1,14 +1,13 @@
 using Argentini.Sfumato.Collections;
+using Microsoft.Extensions.ObjectPool;
 
 namespace Argentini.Sfumato.Tests;
 
 public class ParsingTests
 {
-    [Fact]
-    public void Regex()
-    {
-        var appState = new SfumatoAppState();
-        const string markup = """
+    #region Constants
+    
+    private static string Markup => """
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,34 +24,80 @@ public class ParsingTests
     <div class="block invisible top-8 break-after-auto container aspect-screen elas:aspect-[8/4]"></div>
     <script>
         function test() {
-            let el = document.getElementById('test-element');
-            if (el) {
-                  el.classList.add($`
-                      bg-emerald-900
-                      [font-weight:700]
-                      tabl:[font-weight:700]
-                  `);
-                  el.classList.add(`bg-emerald-950`);
-                  el.classList.add(`[font-weight:600]`);
-                  el.classList.add(`note:[font-weight:600]`);
-            }
+          let el = document.getElementById('test-element');
+          if (el) {
+                el.classList.add($`
+                    bg-emerald-900
+                    [font-weight:700]
+                    tabl:[font-weight:700]
+                `);
+                el.classList.add(`bg-emerald-950`);
+                el.classList.add(`[font-weight:600]`);
+                el.classList.add(`note:[font-weight:600]`);
+          }
         }
     </script>
 </body>
 </html>
-
 """;
-        var matches = appState.ArbitraryCssRegex.Matches(markup);
+    
+    #endregion
+    
+    [Fact]
+    public void MatchArbitraryStylesRegex()
+    {
+        var appState = new SfumatoAppState();
+        var matches = appState.ArbitraryCssRegex.Matches(Markup);
 
-        Assert.Equal(6, matches.Count);        
+        Assert.Equal(6, matches.Count);
+    }
 
-        matches = appState.CoreClassRegex.Matches(markup);
+    [Fact]
+    public void MatchCoreClassesRegex()
+    {
+        var appState = new SfumatoAppState();
+        var matches = appState.CoreClassRegex.Matches(Markup);
 
         Assert.Equal(31, matches.Count);
     }
+
+    [Fact]
+    public void ScssIncludeSfumatoRegex()
+    {
+        var appState = new SfumatoAppState();
+        var matches = appState.SfumatoScssIncludesRegex.Matches(@"@sfumato core;
+");
+
+        Assert.Single(matches);
+        
+        matches = appState.SfumatoScssIncludesRegex.Matches(@"
+@sfumato core;
+");
+
+        Assert.Single(matches);
+        
+        matches = appState.SfumatoScssIncludesRegex.Matches(@"
+@sfumato     core;
+");
+
+        Assert.Single(matches);
+        
+        matches = appState.SfumatoScssIncludesRegex.Matches(@"
+@sfumato     core ;
+");
+
+        Assert.Single(matches);
+        
+        matches = appState.SfumatoScssIncludesRegex.Matches(@"
+/* This is a test */
+@sfumato     core ;
+");
+
+        Assert.Empty(matches);
+    }
     
     [Fact]
-    public void ScssClassCollection()
+    public void GetAllByClassName()
     {
         var scssClassCollection = new ScssClassCollection();
 
@@ -61,65 +106,85 @@ public class ParsingTests
         Assert.Single(scssClassCollection.GetAllByClassName("dark:tabp:hover:bg-slate-100"));
         Assert.Single(scssClassCollection.GetAllByClassName("dark:tabp:hover:bg-slate-100[--my-value]"));
         Assert.Single(scssClassCollection.GetAllByClassName("break-after-auto"));
+        Assert.Equal("bg-slate-100", scssClassCollection.GetAllByClassName("dark:tabp:hover:bg-slate-100[--my-value]").First().RootClassName);
     }
-    
-//     [Fact]
-//     public async Task GenerateColorsCode()
-//     {
-//         var sb = new StringBuilder();
-//         var scss = await File.ReadAllTextAsync(Path.Combine("scss", "text-color.scss"));
-//
-//         if (string.IsNullOrEmpty(scss))
-//             Assert.Fail();
-//
-//         var index = 0;
-//
-//         while (index > -1)
-//         {
-//             if (scss[index] != '.')
-//             {
-//                 index = scss.IndexOf("\n.", index, StringComparison.Ordinal);
-//
-//                 if (index > -1)
-//                     index++;
-//             }
-//
-//             if (index < 0)
-//                 continue;
-// 				
-//             var end = scss.IndexOf(' ', index);
-//
-//             if (end < 0)
-//                 continue;
-//
-//             var propertyPrefix = "color:";
-//             var valueStartIndex = scss.IndexOf(propertyPrefix, end, StringComparison.Ordinal);
-//
-//             if (valueStartIndex > -1)
-//             {
-//                 valueStartIndex += propertyPrefix.Length;
-//                 
-//                 var valueEndIndex = scss.IndexOf(";", valueStartIndex, StringComparison.Ordinal);
-//
-//                 if (valueEndIndex > -1)
-//                 {
-//                     var defaultValue = scss[valueStartIndex..valueEndIndex].Trim();
-//                     
-//                     sb.Append(
-//                         @$"[""{scss.Substring(index + 1, end - index - 1)}""] = new ScssClass
-// {{
-//     ValueMask = ""{{value}}"",
-//     Value = ""{defaultValue}"",
-//     ValueType = ""color"",
-//     Template = ""background-color: {{value}};""
-// }},
-// ");            
-//                 }
-//             }
-//
-//             index = end;
-//         }
-//         
-//         Assert.NotEmpty(sb.ToString());
-//     }
+
+    [Fact]
+    public void ReorderPrefixes()
+    {
+        Assert.Equal("bg-slate-100", SfumatoAppState.ReOrderPrefixes("bg-slate-100"));
+        Assert.Equal("bg-slate-100[--my-value]", SfumatoAppState.ReOrderPrefixes("bg-slate-100[--my-value]"));
+        Assert.Equal("font-2/2", SfumatoAppState.ReOrderPrefixes("font-2/2"));
+        Assert.Equal("tabp:bg-slate-100", SfumatoAppState.ReOrderPrefixes("tabp:bg-slate-100"));
+        Assert.Equal("tabp:hover:bg-slate-100", SfumatoAppState.ReOrderPrefixes("tabp:hover:bg-slate-100"));
+        Assert.Equal("tabp:hover:bg-slate-100", SfumatoAppState.ReOrderPrefixes("hover:tabp:bg-slate-100"));
+        Assert.Equal("dark:tabp:hover:focus:bg-slate-100[--my-value]", SfumatoAppState.ReOrderPrefixes("hover:tabp:note:focus:dark:elas:bg-slate-100[--my-value]"));
+    }
+
+    [Fact]
+    public void EscapeCssClassName()
+    {
+        var pool = new DefaultObjectPoolProvider().CreateStringBuilderPool();
+        
+        Assert.Equal("text-base", "text-base".EscapeCssClassName(pool));
+        Assert.Equal(@"dark\:tabp\:text-base\/5", "dark:tabp:text-base/5".EscapeCssClassName(pool));
+        Assert.Equal(@"dark\:tabp\:text-base\/\[3rem\]", "dark:tabp:text-base/[3rem]".EscapeCssClassName(pool));
+    }
+
+    [Fact]
+    public void GetUserClassValueType()
+    {
+        Assert.Equal(string.Empty, "text-base".GetUserClassValueType());
+        Assert.Equal(string.Empty, "dark:tabp:[width:3rem]".GetUserClassValueType());
+
+        Assert.Equal("length", "dark:tabp:text-base/[3rem]".GetUserClassValueType());
+        Assert.Equal("length", "dark:tabp:p-[3rem]".GetUserClassValueType());
+
+        foreach (var unit in SfumatoScss.CssUnits)
+        {
+            Assert.Equal("length", $"dark:tabp:p-[3{unit}]".GetUserClassValueType());
+        }
+
+        Assert.Equal("flex", "dark:tabp:p-[3fr]".GetUserClassValueType());
+        Assert.Equal("percentage", "dark:tabp:p-[3%]".GetUserClassValueType());
+        Assert.Equal("percentage", "dark:tabp:p-[3.5%]".GetUserClassValueType());
+        Assert.Equal("integer", "dark:tabp:p-[3]".GetUserClassValueType());
+        Assert.Equal("number", "dark:tabp:p-[0.5]".GetUserClassValueType());
+        Assert.Equal("number", "dark:tabp:p-[3.0]".GetUserClassValueType());
+        Assert.Equal("color", "dark:tabp:bg-[#123]".GetUserClassValueType());
+        Assert.Equal("color", "dark:tabp:bg-[#123f]".GetUserClassValueType());
+        Assert.Equal("color", "dark:tabp:bg-[#aa1122]".GetUserClassValueType());
+        Assert.Equal("color", "dark:tabp:bg-[#aa1122ff]".GetUserClassValueType());
+        Assert.Equal("color", "dark:tabp:bg-[rgb(1,2,3)]".GetUserClassValueType());
+        Assert.Equal("color", "dark:tabp:bg-[rgba(1,2,3,0.5)]".GetUserClassValueType());
+        Assert.Equal("string", "dark:tabp:content['hello_world!']".GetUserClassValueType());
+        Assert.Equal("url", "dark:tabp:bg-[url(http://image.src)]".GetUserClassValueType());
+        Assert.Equal("url", "dark:tabp:bg-[http://sfumato.org/images/file.jpg]".GetUserClassValueType());
+        Assert.Equal("url", "dark:tabp:bg-[https://sfumato.org/images/file.jpg]".GetUserClassValueType());
+        Assert.Equal("url", "dark:tabp:bg-[url(/images/file.jpg)]".GetUserClassValueType());
+        Assert.Equal("url", "dark:tabp:bg-[/images/file.jpg]".GetUserClassValueType());
+        
+        foreach (var unit in SfumatoScss.CssAngleUnits)
+        {
+            Assert.Equal("angle", $"dark:tabp:rotate-[1{unit}]".GetUserClassValueType());
+        }
+
+        foreach (var unit in SfumatoScss.CssTimeUnits)
+        {
+            Assert.Equal("time", $"dark:tabp:duration-[100{unit}]".GetUserClassValueType());
+        }
+
+        foreach (var unit in SfumatoScss.CssFrequencyUnits)
+        {
+            Assert.Equal("frequency", $"dark:tabp:volume-[1{unit}]".GetUserClassValueType());
+        }
+
+        foreach (var unit in SfumatoScss.CssResolutionUnits)
+        {
+            Assert.Equal("resolution", $"dark:tabp:width-[1024{unit}]".GetUserClassValueType());
+        }
+        
+        Assert.Equal("ratio", "dark:tabp:width-[1/2]".GetUserClassValueType());
+        Assert.Equal("ratio", "dark:tabp:width-[1_/_2]".GetUserClassValueType());
+    }
 }
