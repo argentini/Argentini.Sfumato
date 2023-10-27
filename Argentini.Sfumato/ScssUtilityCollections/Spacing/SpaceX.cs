373c47ae9f41512b372d805c3762f266aaceff46
Michael Argentini <michael@argentini.us>
2023-10-27 16:19:21 -0400

    Spacing classes complete

---
+++ Argentini.Sfumato/ScssUtilityCollections/Spacing/SpaceX.cs
@@ -0,0 +1,60 @@
+namespace Argentini.Sfumato.ScssUtilityCollections.Spacing;
+
+public class SpaceX : ScssUtilityClassGroupBase 
+{
+    public override string SelectorPrefix => "space-x";
+
+    public readonly Dictionary<string, string> StaticUtilities = new()
+    {
+        ["0"] = """
+                & > * + * {
+                    margin-left: 0px;
+                }
+                """,
+        ["px"] = """
+                 & > * + * {
+                     margin-left: 1px;
+                 }
+                 """,
+    }; 
+    
+    public override string GetStyles(CssSelector cssSelector)
+    {
+        if (cssSelector.AppState is null)
+            return string.Empty;
+
+        #region Static Utilities
+        
+        if (StaticUtilities.TryGetValue(cssSelector.CoreSegment, out var styles))
+            return styles;
+        
+        #endregion
+        
+        #region Calculated Utilities
+        
+        if (cssSelector.AppState.LayoutRemUnitOptions.TryGetValue(cssSelector.CoreSegment, out var unit))
+            return $$"""
+                   & > * + * {
+                       margin-left: {{unit}};
+                   }
+                   """;
+
+        #endregion
+        
+        #region Arbitrary Values
+        
+        if (cssSelector is not { HasArbitraryValue: true, CoreSegment: "" })
+            return string.Empty;
+        
+        if (cssSelector.ArbitraryValueType is "length" or "percentage")
+            return $$"""
+                   & > * + * {
+                       margin-left: {{cssSelector.ArbitraryValue}};
+                   }
+                   """;
+      
+        #endregion
+
+        return string.Empty;
+    }
+}
---
+++ Argentini.Sfumato/ScssUtilityCollections/Spacing/SpaceY.cs
@@ -0,0 +1,60 @@
+namespace Argentini.Sfumato.ScssUtilityCollections.Spacing;
+
+public class SpaceY : ScssUtilityClassGroupBase 
+{
+    public override string SelectorPrefix => "space-y";
+
+    public readonly Dictionary<string, string> StaticUtilities = new()
+    {
+        ["0"] = """
+                & > * + * {
+                    margin-top: 0px;
+                }
+                """,
+        ["px"] = """
+                 & > * + * {
+                     margin-top: 1px;
+                 }
+                 """,
+    }; 
+    
+    public override string GetStyles(CssSelector cssSelector)
+    {
+        if (cssSelector.AppState is null)
+            return string.Empty;
+
+        #region Static Utilities
+        
+        if (StaticUtilities.TryGetValue(cssSelector.CoreSegment, out var styles))
+            return styles;
+        
+        #endregion
+        
+        #region Calculated Utilities
+        
+        if (cssSelector.AppState.LayoutRemUnitOptions.TryGetValue(cssSelector.CoreSegment, out var unit))
+            return $$"""
+                   & > * + * {
+                       margin-top: {{unit}};
+                   }
+                   """;
+
+        #endregion
+        
+        #region Arbitrary Values
+        
+        if (cssSelector is not { HasArbitraryValue: true, CoreSegment: "" })
+            return string.Empty;
+        
+        if (cssSelector.ArbitraryValueType is "length" or "percentage")
+            return $$"""
+                   & > * + * {
+                       margin-top: {{cssSelector.ArbitraryValue}};
+                   }
+                   """;
+      
+        #endregion
+
+        return string.Empty;
+    }
+}
