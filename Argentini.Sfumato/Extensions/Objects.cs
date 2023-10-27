5738ccd0ce8c56973218c7db90c771d1613a79b6
Michael Argentini <michael@argentini.us>
2023-10-27 13:01:18 -0400

    Loading classes now dynamic based on object traversal

---
+++ Argentini.Sfumato/Extensions/Objects.cs
@@ -0,0 +1,25 @@
+﻿namespace Argentini.Sfumato.Extensions;
+
+public static class Objects
+{
+    /// <summary>
+    /// Get all types that inherit from a base type
+    /// </summary>
+    /// <param name="baseType"></param>
+    /// <param name="excludeSystemAssemblies"></param>
+    /// <returns></returns>
+    public static IEnumerable<Type> GetInheritedTypes(this Type baseType, bool excludeSystemAssemblies = true)
+    {
+        var types = new List<Type>();
+		
+        foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies().Where(a => excludeSystemAssemblies == false || (excludeSystemAssemblies && (a.FullName?.StartsWith("System") ?? false) == false && (a.FullName?.StartsWith("Microsoft") ?? false) == false)))
+        {
+            var subtypes = assembly.GetTypes().Where(theType => theType is { IsClass: true, IsAbstract: false }).ToList();
+            var ts = subtypes.Where(t => t != baseType && t.IsAssignableTo(baseType)).ToList();
+            
+            types.AddRange(ts);
+        }
+		
+        return types;
+    }
+}
