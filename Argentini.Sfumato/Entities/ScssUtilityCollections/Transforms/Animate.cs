164e6d9ddf16d7108541d2d7cff93f2ac220a5f3
Michael Argentini <michael@argentini.us>
2023-11-04 21:54:58 -0400

    Fixed animate styles for ping and pulse

--- Argentini.Sfumato/Entities/ScssUtilityCollections/Transforms/Animate.cs
+++ Argentini.Sfumato/Entities/ScssUtilityCollections/Transforms/Animate.cs
@@ -21,23 +21,23 @@ public class Animate : ScssUtilityClassGroupBase
                    """,
         ["ping"] = """
                    animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
-                   
-                   @keyframes pulse {
-                       0%, 100% {
-                           opacity: 1;
-                       }
-                       50% {
-                           opacity: .5;
+
+                   @keyframes ping {
+                       75%, 100% {
+                           transform: scale(2);
+                           opacity: 0;
                        }
                    }
                    """,
         ["pulse"] = """
                     animation: ping 1s cubic-bezier(0, 0, 0.2, 1) infinite;
                    
-                    @keyframes ping {
-                        75%, 100% {
-                            transform: scale(2);
-                            opacity: 0;
+                    @keyframes pulse {
+                        0%, 100% {
+                            opacity: 1;
+                        }
+                        50% {
+                            opacity: .5;
                         }
                     }
                     """,
