2bc093713dae86bed155b9b25b85a30e0de7600c
Michael Argentini <magic@MacBook-Pro-Hitchens.local>
2023-11-10 21:30:17 -0500

    Using em units for default breakpoints, fixes to breakpoint handling

--- Argentini.Sfumato/Entities/SfumatoSettings/MediaBreakpoints.cs
+++ Argentini.Sfumato/Entities/SfumatoSettings/MediaBreakpoints.cs
@@ -2,93 +2,93 @@ namespace Argentini.Sfumato.Entities.SfumatoSettings;
 
 public sealed class MediaBreakpoints
 {
-    private int _zero;
-    public int Zero
+    private string _zero = "0em";
+    public string Zero
     {
         get => _zero;
         set
         {
             _zero = value switch
             {
-                < 0 => 0,
+                "" => "0em",
                 _ => value
             };
         }
     }
-    private int _phab = 400;
-    public int Phab
+    private string _phab = "25em"; // 400px
+    public string Phab
     {
         get => _phab;
         set
         {
             _phab = value switch
             {
-                < 0 => 400,
+                "" => "25em",
                 _ => value
             };
         }
     }
-    private int _tabp = 540;
-    public int Tabp
+    private string _tabp = "33.75em"; // 540px
+    public string Tabp
     {
         get => _tabp;
         set
         {
             _tabp = value switch
             {
-                < 0 => 540,
+                "" => "33.75em",
                 _ => value
             };
         }
     }
-    private int _tabl = 800;
-    public int Tabl
+    private string _tabl = "50em"; // 800px
+    public string Tabl
     {
         get => _tabl;
         set
         {
             _tabl = value switch
             {
-                < 0 => 800,
+                "" => "50em",
                 _ => value
             };
         }
     }
-    private int _note = 1280;
-    public int Note
+    private string _note = "80em"; // 1280px
+    public string Note
     {
         get => _note;
         set
         {
             _note = value switch
             {
-                < 0 => 1280,
+                "" => "80em",
                 _ => value
             };
         }
     }
-    private int _desk = 1440;
-    public int Desk
+    private string _desk = "90em"; // 1440px
+    public string Desk
     {
         get => _desk;
         set
         {
             _desk = value switch
             {
-                < 0 => 1440,
+                "" => "90em",
                 _ => value
             };
         }
     }
-    private int _elas = 1600;
-    public int Elas
+    private string _elas = "100em"; // 1600px
+    public string Elas
     {
         get => _elas;
         set
         {
             _elas = value switch
             {
-                < 0 => 1600,
+                "" => "100em",
                 _ => value
             };
         }
