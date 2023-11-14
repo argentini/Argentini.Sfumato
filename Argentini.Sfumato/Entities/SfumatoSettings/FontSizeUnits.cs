a1d379781e982b35994f1792893f9954288be8aa
Michael Argentini <magic@MacBook-Pro-Hitchens.local>
2023-11-13 21:44:15 -0500

    Non-scaling UI now the default

--- Argentini.Sfumato/Entities/SfumatoSettings/FontSizeUnits.cs
+++ Argentini.Sfumato/Entities/SfumatoSettings/FontSizeUnits.cs
@@ -2,11 +2,11 @@ namespace Argentini.Sfumato.Entities.SfumatoSettings;
 
 public sealed class FontSizeUnits
 {
-    public string Zero { get; set; } = "4.35vw";
-    public string Phab { get; set; } = "4vw";
-    public string Tabp { get; set; } = "1.6vw";
-    public string Tabl { get; set; } = "1vw";
-    public string Note { get; set; } = "1vw";
-    public string Desk { get; set; } = "1vw";
-    public string Elas { get; set; } = "1vw";
+    public string Zero { get; set; } = "16px";
+    public string Phab { get; set; } = "16px";
+    public string Tabp { get; set; } = "16px";
+    public string Tabl { get; set; } = "16px";
+    public string Note { get; set; } = "16px";
+    public string Desk { get; set; } = "16px";
+    public string Elas { get; set; } = "16px";
 }
