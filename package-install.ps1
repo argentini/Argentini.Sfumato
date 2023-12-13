480d0994aaa2f0f21b9533b7eb42a2e59e808c20
Michael Argentini <magic@MacBook-Pro-Hitchens.local>
2023-12-12 21:54:02 -0500

    Updated test package install scripts

--- package-install.ps1
+++ package-install.ps1
@@ -1,1 +1,1 @@
-dotnet tool install --global argentini.sfumato --add-source Argentini.Sfumato\\nupkg --version 5.0.0 --ignore-failed-sources
+dotnet tool install --global argentini.sfumato --add-source Argentini.Sfumato\\nupkg --version 5.0.20 --ignore-failed-sources
