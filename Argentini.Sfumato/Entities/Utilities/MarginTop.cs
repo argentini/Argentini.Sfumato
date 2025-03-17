4e36c37309f310a0414254726411d8721376b997
Michael Argentini <michael@argentini.us>
2025-03-16 22:56:59 -0400

    Added placeholder class for margin-top

---
+++ Argentini.Sfumato/Entities/Utilities/MarginTop.cs
@@ -0,0 +1,15 @@
+namespace Argentini.Sfumato.Entities.Utilities;
+
+public sealed class MarginTop: BaseUtility
+{
+    public override string Prefix => "mt";
+
+    public MarginTop()
+    {
+    }
+    
+    
+    
+    
+    
+}
