1d57bc46ee34caa4c1d38da2651bc774d0201289
Michael Argentini <michael@argentini.us>
2025-03-25 20:23:09 -0400

    Benchmark test cleanup

--- Argentini.Sfumato.Tests/BenchmarkTests.cs
+++ Argentini.Sfumato.Tests/BenchmarkTests.cs
@@ -20,51 +20,6 @@ public class BenchmarkTests
         _testOutputHelper = testOutputHelper;
     }
 
-    #region Constants
-
-    private static string Markup => """
-                                    <!DOCTYPE html>
-                                    <html lang="en">
-                                    <head>
-                                        <meta charset="UTF-8">
-                                        <title>Sample Website</title>
-                                        <meta name="viewport" content="width=device-width, initial-scale=1">
-                                        <link rel="stylesheet" href="css/sfumato.css">
-                                    </head>
-                                    <body class="text-base/5 xl:text-base/[3rem] [-webkit-backdrop-filter:blur(1rem)]">
-                                        <div id="test-home" class="text-[1rem] lg:text-[1.25rem] bg-fuchsia-500 dark:bg-fuchsia-300 dark:text-[length:1rem] xl:text-[#112233] xl:text-[red] xl:text-[--my-color-var] xl:text-[var(--my-color-var)]">
-                                            <p class="[font-weight:900] sm:[font-weight:900]">Placeholder</p>
-                                            <p class="[fontweight:400] sm:[fontweight:300] xl:text[#112233] xl:text-slate[#112233] xl:text-slate-50[#112233] xxl:text-slate-50-[#112233]">Invalid Classes</p>
-                                        </div>
-                                        <div class="content-['Hello!'] block invisible top-8 break-after-auto container aspect-screen xxl:aspect-[8/4]"></div>
-                                        <script>
-                                            function test() {
-                                              let el = document.getElementById('test-element');
-                                              if (el) {
-                                                    el.classList.add($`
-                                                        bg-emerald-900
-                                                        [font-weight:700]
-                                                        md:[font-weight:700]
-                                                    `);
-                                                    el.classList.add(`bg-emerald-950`);
-                                                    el.classList.add(`[font-weight:600]`);
-                                                    el.classList.add(`lg:[font-weight:600]`);
-                                              }
-                                            }
-                                        </script>
-                                        @{
-                                            var test1 = $""
-                                                block bg-slate-400
-                                            "";
-                                            
-                                            var detailsMask = $"<span class=\"line-clamp-1 mt-1 text-slate-500 dark:text-dark-foreground-dim line-clamp-2\"><span class=""line-clamp-2"">{description}</span></span>";
-                                        }
-                                    </body>
-                                    </html>
-                                    """;
-    
-    #endregion
-
     [Fact]
     public void ScanContentForClassesBenchmark()
     {
@@ -94,7 +49,7 @@ public class NanosecondsConfig : ManualConfig
     public NanosecondsConfig()
     {
         AddColumn(StatisticColumn.Mean, StatisticColumn.Error, StatisticColumn.StdDev);
-        SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithTimeUnit(TimeUnit.Nanosecond);
+        SummaryStyle = SummaryStyle.Default.WithTimeUnit(TimeUnit.Nanosecond);
     }
 }
 public class MicrosecondsConfig : ManualConfig
@@ -102,7 +57,7 @@ public class MicrosecondsConfig : ManualConfig
     public MicrosecondsConfig()
     {
         AddColumn(StatisticColumn.Mean, StatisticColumn.Error, StatisticColumn.StdDev);
-        SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithTimeUnit(TimeUnit.Microsecond);
+        SummaryStyle = SummaryStyle.Default.WithTimeUnit(TimeUnit.Microsecond);
     }
 }
 public class MillisecondsConfig : ManualConfig
@@ -110,6 +65,6 @@ public class MillisecondsConfig : ManualConfig
     public MillisecondsConfig()
     {
         AddColumn(StatisticColumn.Mean, StatisticColumn.Error, StatisticColumn.StdDev);
-        SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithTimeUnit(TimeUnit.Millisecond);
+        SummaryStyle = SummaryStyle.Default.WithTimeUnit(TimeUnit.Millisecond);
     }
 }
