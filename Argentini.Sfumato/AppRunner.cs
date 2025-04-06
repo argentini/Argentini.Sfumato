0b6257b6ee03bfa0b672106d4b20dc144aa3273b
Michael Argentini <michael@argentini.us>
2025-04-06 18:25:04 -0400

    Refactored AppRunnerSettings processing

--- Argentini.Sfumato/AppRunner.cs
+++ Argentini.Sfumato/AppRunner.cs
@@ -7,25 +7,25 @@ public partial class AppRunner
 	#region Regular Expressions
 
 	[GeneratedRegex(@"(/\*[\d\D]*?\*/)", RegexOptions.Compiled)]
-	private static partial Regex RemoveBlockCommentsRegex();
+	public static partial Regex RemoveBlockCommentsRegex();
 	
 	[GeneratedRegex(@"::sfumato\s*\{(?:(?>[^{}]+)|\{(?<bal>)|\}(?<-bal>))*(?(bal)(?!))\}", RegexOptions.Compiled | RegexOptions.Singleline)]
-	private static partial Regex SfumatoCssBlockRegex();
+	public static partial Regex SfumatoCssBlockRegex();
 
 	[GeneratedRegex(@"((?<property>--[\w-]+)\s*:\s*(?<value>(?:""(?:\\.|[^""\\])*""|'(?:\\.|[^'\\])*'|[^;])+)\s*;)", RegexOptions.Compiled)]
-	private static partial Regex CssCustomPropertiesRegex();
+	public static partial Regex CssCustomPropertiesRegex();
 
 	[GeneratedRegex(@"@keyframes\s+(?<name>[a-zA-Z0-9_-]+)\s*\{(?:(?>[^{}]+)|\{(?<bal>)|\}(?<-bal>))*(?(bal)(?!))\}", RegexOptions.Compiled | RegexOptions.Singleline)]
-	private static partial Regex CssKeyframeBlocksRegex();
+	public static partial Regex CssKeyframeBlocksRegex();
 
 	[GeneratedRegex(@"\s+", RegexOptions.Compiled)]
-	private static partial Regex ConsolidateSpacesRegex();
+	public static partial Regex ConsolidateSpacesRegex();
 	
 	[GeneratedRegex(@"\s+(?=\r\n|\n)", RegexOptions.Compiled)]
-	private static partial Regex WhitespaceBeforeLineBreakRegex();
+	public static partial Regex WhitespaceBeforeLineBreakRegex();
 	
 	[GeneratedRegex(@"(?:\r\n|\n){3,}", RegexOptions.Compiled)]
-	private static partial Regex ConsolidateLineBreaksRegex();
+	public static partial Regex ConsolidateLineBreaksRegex();
 	
 	#endregion
 	
@@ -55,101 +55,15 @@ public partial class AppRunner
     {
 	    try
 	    {
-		    AppRunnerSettings.CssContent = WhitespaceBeforeLineBreakRegex().Replace(AppRunnerSettings.CssContent, string.Empty);
+		    AppRunnerSettings.ExtractCssContent();
+		    AppRunnerSettings.ExtractSfumatoItems();
+		    AppRunnerSettings.ProcessProjectSettings();
 
-		    var quoteMatches = SfumatoCssBlockRegex().Matches(AppRunnerSettings.CssContent);
 
-		    if (quoteMatches.Count == 0)
-		    {
-			    await Console.Out.WriteLineAsync($"{AppState.CliErrorPrefix}No ::sfumato {{}} block in file: {AppRunnerSettings.CssFilePath}");
-			    Environment.Exit(1);
-		    }
 
-		    if (quoteMatches.Count != 1)
-		    {
-			    await Console.Out.WriteLineAsync($"{AppState.CliErrorPrefix}Only one ::sfumato {{}} block allowed in file: {AppRunnerSettings.CssFilePath}");
-			    Environment.Exit(1);
-		    }
 
-		    AppRunnerSettings.SfumatoCssBlock = quoteMatches[0].Value;
 
-		    var lineBreaks = AppRunnerSettings.CssContent.Contains("\r\n") ? "\r\n\r\n" : "\n\n";
-		    
-		    AppRunnerSettings.TrimmedCssContent = ConsolidateLineBreaksRegex().Replace(AppRunnerSettings.CssContent.Replace(AppRunnerSettings.SfumatoCssBlock, string.Empty), lineBreaks);
-		    
-		    var sfumatoCssBlock = AppRunnerSettings.SfumatoCssBlock.Trim()[AppRunnerSettings.SfumatoCssBlock.IndexOf('{')..].TrimEnd('}').Trim();
 
-		    sfumatoCssBlock = RemoveBlockCommentsRegex().Replace(sfumatoCssBlock, string.Empty);
-
-		    quoteMatches = CssCustomPropertiesRegex().Matches(sfumatoCssBlock);
-
-		    foreach (Match match in quoteMatches)
-		    {
-			    AppRunnerSettings.SfumatoBlockItems.Add(match.Value[..match.Value.IndexOf(':')].Trim(), match.Value[(match.Value.IndexOf(':') + 1)..].TrimEnd(';').Trim());
-		    }
-		    
-		    quoteMatches = CssKeyframeBlocksRegex().Matches(sfumatoCssBlock);
-
-		    foreach (Match match in quoteMatches)
-		    {
-			    AppRunnerSettings.SfumatoBlockItems.Add(match.Value[..match.Value.IndexOf('{')].Trim(), match.Value[match.Value.IndexOf('{')..].TrimEnd(';').Trim());
-		    }
-
-		    if (AppRunnerSettings.SfumatoBlockItems.Count == 0)
-		    {
-			    await Console.Out.WriteLineAsync($"{AppState.CliErrorPrefix}No options specified in file: {AppRunnerSettings.CssFilePath}");
-			    Environment.Exit(1);
-		    }
-
-		    if (AppRunnerSettings.SfumatoBlockItems.TryGetValue("--use-reset", out var useReset))
-			    AppRunnerSettings.UseReset = useReset.Equals("true", StringComparison.Ordinal);
-
-		    if (AppRunnerSettings.SfumatoBlockItems.TryGetValue("--use-forms", out var useForms))
-			    AppRunnerSettings.UseForms = useForms.Equals("true", StringComparison.Ordinal);
-
-		    if (AppRunnerSettings.UseMinify == false && AppRunnerSettings.SfumatoBlockItems.TryGetValue("--use-minify", out var useMinify))
-			    AppRunnerSettings.UseMinify = useMinify.Equals("true", StringComparison.Ordinal);
-
-		    if (AppRunnerSettings.SfumatoBlockItems.TryGetValue("--output-path", out var outputPath))
-			    AppRunnerSettings.CssOutputFilePath = string.IsNullOrEmpty(outputPath) ? "sfumato.css" : outputPath;
-
-		    if (AppRunnerSettings.SfumatoBlockItems.TryGetValue("--paths", out var pathsValue))
-		    {
-			    var paths = ConsolidateSpacesRegex().Replace(pathsValue, " ").TrimStart('[').TrimEnd(']').Trim().Replace("\", \"", "\",\"").Split("\",\"", StringSplitOptions.RemoveEmptyEntries);
-
-			    if (paths.Length != 0)
-			    {
-				    foreach (var p in paths)
-					    AppRunnerSettings.Paths.Add(p.Trim('\"'));
-			    }
-		    }
-
-		    if (AppRunnerSettings.SfumatoBlockItems.TryGetValue("--not-paths", out var notPathsValue))
-		    {
-			    var notPaths = ConsolidateSpacesRegex().Replace(notPathsValue, " ").TrimStart('[').TrimEnd(']').Trim().Replace("\", \"", "\",\"").Split("\",\"", StringSplitOptions.RemoveEmptyEntries);
-
-			    if (notPaths.Length != 0)
-			    {
-				    foreach (var p in notPaths)
-					    AppRunnerSettings.NotPaths.Add(p.Trim('\"'));
-			    }
-		    }
-
-		    #region Validate Settings
-
-		    if (string.IsNullOrEmpty(AppRunnerSettings.CssOutputFilePath))
-		    {
-			    await Console.Out.WriteLineAsync($"{AppState.CliErrorPrefix}Must specify --output-path in file: {AppRunnerSettings.CssFilePath}");
-			    Environment.Exit(1);
-		    }
-
-		    if (AppRunnerSettings.Paths.Count == 0)
-		    {
-			    await Console.Out.WriteLineAsync($"{AppState.CliErrorPrefix}Must specify --paths: [] in file: {AppRunnerSettings.CssFilePath}");
-			    Environment.Exit(1);
-		    }
-
-		    #endregion
 	    }
 	    catch (Exception e)
 	    {
--- Argentini.Sfumato/AppRunnerSettings.cs
+++ Argentini.Sfumato/AppRunnerSettings.cs
@@ -16,5 +16,124 @@ public sealed class AppRunnerSettings
     public string SfumatoCssBlock { get; set; } = string.Empty;
     public string TrimmedCssContent { get; set; } = string.Empty;
     public Dictionary<string, string> SfumatoBlockItems { get; } = [];
+
+    public void ExtractCssContent()
+    {
+	    try
+	    {
+	        CssContent = AppRunner.WhitespaceBeforeLineBreakRegex().Replace(CssContent, string.Empty);
+	        CssContent = AppRunner.RemoveBlockCommentsRegex().Replace(CssContent, string.Empty);
+				    
+	        var quoteMatches = AppRunner.SfumatoCssBlockRegex().Matches(CssContent);
+
+	        if (quoteMatches.Count == 0)
+	        {
+	            Console.WriteLine($"{AppState.CliErrorPrefix}No ::sfumato {{}} block in file: {CssFilePath}");
+	            Environment.Exit(1);
+	        }
+
+	        if (quoteMatches.Count != 1)
+	        {
+	            Console.WriteLine($"{AppState.CliErrorPrefix}Only one ::sfumato {{}} block allowed in file: {CssFilePath}");
+	            Environment.Exit(1);
+	        }
+
+	        SfumatoCssBlock = quoteMatches[0].Value;
+
+	        var lineBreaks = CssContent.Contains("\r\n") ? "\r\n\r\n" : "\n\n";
+			    
+	        TrimmedCssContent = AppRunner.ConsolidateLineBreaksRegex().Replace(CssContent.Replace(SfumatoCssBlock, string.Empty), lineBreaks);
+	    }
+	    catch (Exception e)
+	    {
+		    Console.WriteLine($"{AppState.CliErrorPrefix}{e.Message}");
+		    Environment.Exit(1);
+	    }
+    }
     
+    public void ExtractSfumatoItems()
+    {
+	    try
+	    {
+	        var sfumatoCssBlock = SfumatoCssBlock.Trim()[SfumatoCssBlock.IndexOf('{')..].TrimEnd('}').Trim();
+
+	        var quoteMatches = AppRunner.CssCustomPropertiesRegex().Matches(sfumatoCssBlock);
+
+	        foreach (Match match in quoteMatches)
+	            SfumatoBlockItems.Add(match.Value[..match.Value.IndexOf(':')].Trim(), match.Value[(match.Value.IndexOf(':') + 1)..].TrimEnd(';').Trim());
+			    
+	        quoteMatches = AppRunner.CssKeyframeBlocksRegex().Matches(sfumatoCssBlock);
+
+	        foreach (Match match in quoteMatches)
+	            SfumatoBlockItems.Add(match.Value[..match.Value.IndexOf('{')].Trim(), match.Value[match.Value.IndexOf('{')..].TrimEnd(';').Trim());
+
+	        if (SfumatoBlockItems.Count > 0)
+	            return;
+	        
+	        Console.WriteLine($"{AppState.CliErrorPrefix}No options specified in file: {CssFilePath}");
+	        Environment.Exit(1);
+	    }
+	    catch (Exception e)
+	    {
+		    Console.WriteLine($"{AppState.CliErrorPrefix}{e.Message}");
+		    Environment.Exit(1);
+	    }
+    }
+
+    public void ProcessProjectSettings()
+    {
+	    try
+	    {
+		    if (SfumatoBlockItems.TryGetValue("--use-reset", out var useReset))
+			    UseReset = useReset.Equals("true", StringComparison.Ordinal);
+
+		    if (SfumatoBlockItems.TryGetValue("--use-forms", out var useForms))
+			    UseForms = useForms.Equals("true", StringComparison.Ordinal);
+
+		    if (UseMinify == false && SfumatoBlockItems.TryGetValue("--use-minify", out var useMinify))
+			    UseMinify = useMinify.Equals("true", StringComparison.Ordinal);
+
+		    if (SfumatoBlockItems.TryGetValue("--output-path", out var outputPath))
+			    CssOutputFilePath = string.IsNullOrEmpty(outputPath) ? "sfumato.css" : outputPath;
+
+		    if (SfumatoBlockItems.TryGetValue("--paths", out var pathsValue))
+		    {
+			    var paths = AppRunner.ConsolidateSpacesRegex().Replace(pathsValue, " ").TrimStart('[').TrimEnd(']').Trim().Replace("\", \"", "\",\"").Split("\",\"", StringSplitOptions.RemoveEmptyEntries);
+
+			    if (paths.Length != 0)
+			    {
+				    foreach (var p in paths)
+					    Paths.Add(p.Trim('\"'));
+			    }
+		    }
+
+		    if (SfumatoBlockItems.TryGetValue("--not-paths", out var notPathsValue))
+		    {
+			    var notPaths = AppRunner.ConsolidateSpacesRegex().Replace(notPathsValue, " ").TrimStart('[').TrimEnd(']').Trim().Replace("\", \"", "\",\"").Split("\",\"", StringSplitOptions.RemoveEmptyEntries);
+
+			    if (notPaths.Length != 0)
+			    {
+				    foreach (var p in notPaths)
+					    NotPaths.Add(p.Trim('\"'));
+			    }
+		    }
+
+		    if (string.IsNullOrEmpty(CssOutputFilePath))
+		    {
+			    Console.WriteLine($"{AppState.CliErrorPrefix}Must specify --output-path in file: {CssFilePath}");
+			    Environment.Exit(1);
+		    }
+
+		    if (Paths.Count > 0)
+			    return;
+		    
+		    Console.WriteLine($"{AppState.CliErrorPrefix}Must specify --paths: [] in file: {CssFilePath}");
+		    Environment.Exit(1);
+	    }
+	    catch (Exception e)
+	    {
+		    Console.WriteLine($"{AppState.CliErrorPrefix}{e.Message}");
+		    Environment.Exit(1);
+	    }
+    }
 }
