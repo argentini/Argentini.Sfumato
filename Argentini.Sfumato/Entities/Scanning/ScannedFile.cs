ff3d42e476622200936769dac5d6b1c13bfdd544
Michael Argentini <michael@argentini.us>
2025-04-10 17:56:35 -0400

    Added ScannedFile.LoadAndScanFileAsync()

--- Argentini.Sfumato/Entities/Scanning/ScannedFile.cs
+++ Argentini.Sfumato/Entities/Scanning/ScannedFile.cs
@@ -1,14 +1,43 @@
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
 using Argentini.Sfumato.Entities.CssClassProcessing;
 
 namespace Argentini.Sfumato.Entities.Scanning;
 
 public sealed class ScannedFile
 {
-    public string AbsoluteFilePath { get; set; } = string.Empty;
-    public string FileContent { get; set; } = string.Empty;
+    private string _absoluteFilePath = string.Empty;
+    public string AbsoluteFilePath
+    {
+        get => _absoluteFilePath;
+        set
+        {
+            _absoluteFilePath = value;
 
-    public string FileName => Path.GetFileName(AbsoluteFilePath) ?? string.Empty;
-    public string FilePath => Path.GetDirectoryName(AbsoluteFilePath) ?? string.Empty;
+            FileName = Path.GetFileName(value);
+            FilePath = Path.GetDirectoryName(value) ?? string.Empty;
+        }
+    }
     
+    public string FileContent { get; set; } = string.Empty;
+    public string FileName { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
+
+    public bool IsValid { get; set; }
+
     public Dictionary<string,CssClass> UtilityClasses { get; set; } = new(StringComparer.Ordinal);
+
+    public ScannedFile(string filePath)
+    {
+        AbsoluteFilePath = filePath;
+    }
+    
+    public async Task LoadAndScanFileAsync(AppRunner appRunner)
+    {
+        FileContent = await Storage.ReadAllTextWithRetriesAsync(AbsoluteFilePath, 5000);
+        UtilityClasses = ContentScanner.ScanFileForUtilityClasses(FileContent, appRunner);
+    }
 }
