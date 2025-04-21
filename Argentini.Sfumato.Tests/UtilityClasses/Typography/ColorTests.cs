4463904adb1f6c86a81a21f85d5b3977db438d8e
Michael Argentini <michael@argentini.us>
2025-04-21 18:54:35 -0400

    Started utility class tests

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Typography/ColorTests.cs
@@ -0,0 +1,96 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Typography;
+
+public class ColorTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void Color()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        appRunner.Library.ColorsByName.Add("fynydd-hex", "#0088ff");
+        appRunner.Library.ColorsByName.Add("fynydd-rgb", "rgba(0, 136, 255, 1.0)");
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "text-lime-800",
+                EscapedClassName = ".text-lime-800",
+                Styles =
+                    """
+                    color: var(--color-lime-800);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--color-lime-800" ],
+            },
+            new ()
+            {
+                ClassName = "text-lime-800/37",
+                EscapedClassName = @".text-lime-800\/37",
+                Styles =
+                    """
+                    color: color-mix(in oklab, var(--color-lime-800) 37%, transparent);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--color-lime-800" ],
+            },
+            new ()
+            {
+                ClassName = "text-fynydd-hex/37",
+                EscapedClassName = @".text-fynydd-hex\/37",
+                Styles =
+                    """
+                    color: color-mix(in srgb, var(--color-fynydd-hex) 37%, transparent);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--color-fynydd-hex" ],
+            },
+            new ()
+            {
+                ClassName = "text-fynydd-rgb/37",
+                EscapedClassName = @".text-fynydd-rgb\/37",
+                Styles =
+                    """
+                    color: color-mix(in srgb, var(--color-fynydd-rgb) 37%, transparent);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--color-fynydd-rgb" ],
+            },
+            new ()
+            {
+                ClassName = "text-lime-800!",
+                EscapedClassName = @".text-lime-800\!",
+                Styles =
+                    """
+                    color: var(--color-lime-800) !important;
+                    """,
+                IsValid = true,
+                IsImportant = true,
+                UsedCssCustomProperties = [ "--color-lime-800" ],
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"Typography / Color => {test.ClassName}");
+        }
+    }
+}
---
+++ Argentini.Sfumato.Tests/UtilityClasses/Typography/FontSizeTests.cs
@@ -0,0 +1,78 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Typography;
+
+public class FontSizeTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void FontSize()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "text-base",
+                EscapedClassName = ".text-base",
+                Styles =
+                    """
+                    font-size: var(--text-base);
+                    line-height: var(--text-base--line-height, initial);
+                    letter-spacing: var(--text-base--letter-spacing, initial);
+                    font-weight: var(--text-base--font-weight, initial);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing", "--text-base", "--text-base--line-height", "--text-base--letter-spacing", "--text-base--font-weight" ],
+            },
+            new ()
+            {
+                ClassName = "text-base/6",
+                EscapedClassName = @".text-base\/6",
+                Styles =
+                    """
+                    font-size: var(--text-base);
+                    line-height: calc(var(--spacing) * 6);
+                    letter-spacing: var(--text-base--letter-spacing, initial);
+                    font-weight: var(--text-base--font-weight, initial);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing", "--text-base", "--text-base--line-height", "--text-base--letter-spacing", "--text-base--font-weight" ],
+            },
+            new ()
+            {
+                ClassName = "text-base/[1.35rem]",
+                EscapedClassName = @".text-base\/\[1\.35rem\]",
+                Styles =
+                    """
+                    font-size: var(--text-base);
+                    line-height: 1.35rem;
+                    letter-spacing: var(--text-base--letter-spacing, initial);
+                    font-weight: var(--text-base--font-weight, initial);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing", "--text-base", "--text-base--line-height", "--text-base--letter-spacing", "--text-base--font-weight" ],
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"Typography / FontSize => {test.ClassName}");
+        }
+    }
+}
