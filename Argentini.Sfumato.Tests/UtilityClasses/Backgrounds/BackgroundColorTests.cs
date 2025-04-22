d2ed8cd1cfbc3c98f0372ccebc541333f0c8dd55
Michael Argentini <michael@argentini.us>
2025-04-21 20:43:26 -0400

    Added BackgroundColor tests

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Backgrounds/BackgroundColorTests.cs
@@ -0,0 +1,96 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Backgrounds;
+
+public class BackgroundColorTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void BackgroundColor()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        appRunner.Library.ColorsByName.Add("fynydd-hex", "#0088ff");
+        appRunner.Library.ColorsByName.Add("fynydd-rgb", "rgba(0, 136, 255, 1.0)");
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "bg-lime-800",
+                EscapedClassName = ".bg-lime-800",
+                Styles =
+                    """
+                    background-color: var(--color-lime-800);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--color-lime-800" ],
+            },
+            new ()
+            {
+                ClassName = "bg-lime-800/37",
+                EscapedClassName = @".bg-lime-800\/37",
+                Styles =
+                    """
+                    background-color: color-mix(in oklab, var(--color-lime-800) 37%, transparent);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--color-lime-800" ],
+            },
+            new ()
+            {
+                ClassName = "bg-fynydd-hex/37",
+                EscapedClassName = @".bg-fynydd-hex\/37",
+                Styles =
+                    """
+                    background-color: color-mix(in srgb, var(--color-fynydd-hex) 37%, transparent);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--color-fynydd-hex" ],
+            },
+            new ()
+            {
+                ClassName = "bg-fynydd-rgb/37",
+                EscapedClassName = @".bg-fynydd-rgb\/37",
+                Styles =
+                    """
+                    background-color: color-mix(in srgb, var(--color-fynydd-rgb) 37%, transparent);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--color-fynydd-rgb" ],
+            },
+            new ()
+            {
+                ClassName = "bg-lime-800!",
+                EscapedClassName = @".bg-lime-800\!",
+                Styles =
+                    """
+                    background-color: var(--color-lime-800) !important;
+                    """,
+                IsValid = true,
+                IsImportant = true,
+                UsedCssCustomProperties = [ "--color-lime-800" ],
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"Backgrounds / BackgroundColor => {test.ClassName}");
+        }
+    }
+}
