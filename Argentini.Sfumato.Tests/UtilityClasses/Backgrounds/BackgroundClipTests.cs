65fa5452f18262c0c8a804befc142e78be7632e4
Michael Argentini <michael@argentini.us>
2025-04-29 20:49:16 -0400

    Added BackgroundClip, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Backgrounds/BackgroundClipTests.cs
@@ -0,0 +1,55 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Backgrounds;
+
+public class BackgroundClipTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void BackgroundClip()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "bg-clip-padding",
+                EscapedClassName = ".bg-clip-padding",
+                Styles =
+                    """
+                    background-clip: padding-box;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "bg-clip-text",
+                EscapedClassName = ".bg-clip-text",
+                Styles =
+                    """
+                    background-clip: text;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"Backgrounds / BackgroundClip => {test.ClassName}");
+        }
+    }
+}
