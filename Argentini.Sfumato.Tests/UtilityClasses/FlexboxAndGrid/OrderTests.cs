823da9e2bb3b08ede8de4f5a44074acdbce30e6c
Michael Argentini <michael@argentini.us>
2025-04-30 16:36:54 -0400

    Added Order, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/FlexboxAndGrid/OrderTests.cs
@@ -0,0 +1,132 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.FlexboxAndGrid;
+
+public class OrderTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void Order()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "order-first",
+                EscapedClassName = ".order-first",
+                Styles =
+                    """
+                    order: calc(-infinity);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "order-last",
+                EscapedClassName = ".order-last",
+                Styles =
+                    """
+                    order: calc(infinity);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "order-none",
+                EscapedClassName = ".order-none",
+                Styles =
+                    """
+                    order: 0;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "order-2",
+                EscapedClassName = ".order-2",
+                Styles =
+                    """
+                    order: 2;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "order-[3]",
+                EscapedClassName = @".order-\[3\]",
+                Styles =
+                    """
+                    order: 3;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "order-(--my-order)",
+                EscapedClassName = @".order-\(--my-order\)",
+                Styles =
+                    """
+                    order: var(--my-order);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "order-(integer:--my-order)",
+                EscapedClassName = @".order-\(integer\:--my-order\)",
+                Styles =
+                    """
+                    order: var(--my-order);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "order-[var(--my-order)]",
+                EscapedClassName = @".order-\[var\(--my-order\)\]",
+                Styles =
+                    """
+                    order: var(--my-order);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "order-[integer:var(--my-order)]",
+                EscapedClassName = @".order-\[integer\:var\(--my-order\)\]",
+                Styles =
+                    """
+                    order: var(--my-order);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
---
+++ Argentini.Sfumato/Entities/UtilityClasses/FlexboxAndGrid/Order.cs
@@ -0,0 +1,60 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.FlexboxAndGrid;
+
+public sealed class Order : ClassDictionaryBase
+{
+    public Order()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "order-", new ClassDefinition
+                {
+                    UsesInteger = true,
+                    Template =
+                        """
+                        order: {0};
+                        """,
+                    ArbitraryCssValueTemplate =
+                        """
+                        order: {0};
+                        """,
+                }
+            },
+            {
+                "order-first", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        order: calc(-infinity);
+                        """,
+                }
+            },
+            {
+                "order-last", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        order: calc(infinity);
+                        """,
+                }
+            },
+            {
+                "order-none", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        order: 0;
+                        """,
+                }
+            },
+        });
+    }
+    
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
