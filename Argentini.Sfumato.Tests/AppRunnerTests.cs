9598ee03282a87abb71b6588bba4b53a65a6d01f
Michael Argentini <michael@argentini.us>
2025-04-30 17:10:02 -0400

    Added GridColumn, test

--- Argentini.Sfumato.Tests/AppRunnerTests.cs
+++ Argentini.Sfumato.Tests/AppRunnerTests.cs
@@ -10,7 +10,7 @@ public class AppRunnerTests
 
         await scannedFile.LoadAndScanFileAsync(appRunner);
         
-        Assert.Equal(125, scannedFile.UtilityClasses.Count);
+        Assert.Equal(128, scannedFile.UtilityClasses.Count);
     }
 
     [Fact]
