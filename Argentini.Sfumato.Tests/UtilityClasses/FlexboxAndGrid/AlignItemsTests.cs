25821a1c127b89d36a946192c8deab8bc4a9dddf
Michael Argentini <michael@argentini.us>
2025-04-30 18:11:19 -0400

    Added AlignItems, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/FlexboxAndGrid/AlignItemsTests.cs
@@ -0,0 +1,66 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.FlexboxAndGrid;
+
+public class AlignItemsTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void AlignItems()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "items-start",
+                EscapedClassName = ".items-start",
+                Styles =
+                    """
+                    align-items: flex-start;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "items-end",
+                EscapedClassName = ".items-end",
+                Styles =
+                    """
+                    align-items: flex-end;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "items-baseline-last",
+                EscapedClassName = ".items-baseline-last",
+                Styles =
+                    """
+                    align-items: last baseline;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
---
+++ Argentini.Sfumato/Entities/UtilityClasses/FlexboxAndGrid/AlignItems.cs
@@ -0,0 +1,96 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.FlexboxAndGrid;
+
+public sealed class AlignItems : ClassDictionaryBase
+{
+    public AlignItems()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "items-start", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-items: flex-start;
+                        """,
+                }
+            },
+            {
+                "items-end", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-items: flex-end;
+                        """,
+                }
+            },
+            {
+                "items-end-safe", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-items: safe flex-end;
+                        """,
+                }
+            },
+            {
+                "items-center", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-items: center;
+                        """,
+                }
+            },
+            {
+                "items-center-safe", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-items: safe center;
+                        """,
+                }
+            },
+            {
+                "items-baseline", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-items: baseline;
+                        """,
+                }
+            },
+            {
+                "items-baseline-last", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-items: last baseline;
+                        """,
+                }
+            },
+            {
+                "items-stretch", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-items: stretch;
+                        """,
+                }
+            },
+        });
+    }
+    
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
