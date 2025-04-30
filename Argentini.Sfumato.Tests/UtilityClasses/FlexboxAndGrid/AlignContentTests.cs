f85905fc5ceb023f80c90a041eedf9f4ec7738b2
Michael Argentini <michael@argentini.us>
2025-04-30 18:07:47 -0400

    Added AlignContent, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/FlexboxAndGrid/AlignContentTests.cs
@@ -0,0 +1,66 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.FlexboxAndGrid;
+
+public class AlignContentTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void AlignContent()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "content-start",
+                EscapedClassName = ".content-start",
+                Styles =
+                    """
+                    align-content: flex-start;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "content-end",
+                EscapedClassName = ".content-end",
+                Styles =
+                    """
+                    align-content: flex-end;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "content-between",
+                EscapedClassName = ".content-between",
+                Styles =
+                    """
+                    align-content: space-between;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
---
+++ Argentini.Sfumato/Entities/UtilityClasses/FlexboxAndGrid/AlignContent.cs
@@ -0,0 +1,106 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.FlexboxAndGrid;
+
+public sealed class AlignContent : ClassDictionaryBase
+{
+    public AlignContent()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "content-normal", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-content: normal;
+                        """,
+                }
+            },
+            {
+                "content-center", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-content: center;
+                        """,
+                }
+            },
+            {
+                "content-start", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-content: flex-start;
+                        """,
+                }
+            },
+            {
+                "content-end", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-content: flex-end;
+                        """,
+                }
+            },
+            {
+                "content-between", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-content: space-between;
+                        """,
+                }
+            },
+            {
+                "content-around", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-content: space-around;
+                        """,
+                }
+            },
+            {
+                "content-evenly", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-content: space-evenly;
+                        """,
+                }
+            },
+            {
+                "content-baseline", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-content: baseline;
+                        """,
+                }
+            },
+            {
+                "content-stretch", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        align-content: stretch;
+                        """,
+                }
+            },
+        });
+    }
+    
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
