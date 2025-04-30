987482ea8dcaa7827bf70f8dfd1501ec552cf91d
Michael Argentini <michael@argentini.us>
2025-04-30 17:28:00 -0400

    Added GridAutoColumns, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/FlexboxAndGrid/GridAutoColumns.cs
@@ -0,0 +1,70 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.FlexboxAndGrid;
+
+public sealed class GridAutoColumns : ClassDictionaryBase
+{
+    public GridAutoColumns()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "auto-cols-", new ClassDefinition
+                {
+                    UsesAbstractValue = true,
+                    Template =
+                        """
+                        grid-auto-columns: {0};
+                        """,
+                    ArbitraryCssValueTemplate =
+                        """
+                        grid-auto-columns: {0};
+                        """,
+                }
+            },
+            {
+                "auto-cols-auto", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        grid-auto-columns: auto;
+                        """,
+                }
+            },
+            {
+                "auto-cols-min", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        grid-auto-columns: min-content;
+                        """,
+                }
+            },
+            {
+                "auto-cols-max", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        grid-auto-columns: max-content;
+                        """,
+                }
+            },
+            {
+                "auto-cols-fr", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        grid-auto-columns: minmax(0, 1fr);
+                        """,
+                }
+            },
+        });
+    }
+    
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
