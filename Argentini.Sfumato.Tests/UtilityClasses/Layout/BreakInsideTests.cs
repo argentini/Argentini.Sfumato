d1a6f96fc73ec5ab3c85a77f49a65ff8d21fa04d
Michael Argentini <michael@argentini.us>
2025-04-29 22:04:46 -0400

    Added BreakInside, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Layout/BreakInsideTests.cs
@@ -0,0 +1,66 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Layout;
+
+public class BreakInsideTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void BreakInside()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "break-inside-auto",
+                EscapedClassName = ".break-inside-auto",
+                Styles =
+                    """
+                    break-inside: auto;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "break-inside-avoid",
+                EscapedClassName = ".break-inside-avoid",
+                Styles =
+                    """
+                    break-inside: avoid;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "break-inside-avoid-page",
+                EscapedClassName = ".break-inside-avoid-page",
+                Styles =
+                    """
+                    break-inside: avoid-page;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"Layout / BreakInside => {test.ClassName}");
+        }
+    }
+}
