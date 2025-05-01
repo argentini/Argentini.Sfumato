ea2e36b78b2616b5217d083170667a5c765532b8
Michael Argentini <michael@argentini.us>
2025-05-01 17:07:03 -0400

    Added OutlineColor, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/Borders/OutlineColor.cs
@@ -0,0 +1,48 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Borders;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public sealed class OutlineColor : ClassDictionaryBase
+{
+    public OutlineColor()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "outline-", new ClassDefinition
+                {
+                    UsesColor = true,
+                    UsesSlashModifier = true,
+                    Template =
+                        """
+                        outline-color: {0};
+                        """,
+                }
+            },
+            {
+                "outline-inherit", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        outline-color: inherit;
+                        """,
+                }
+            },
+            {
+                "outline-current", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        outline-color: currentColor;
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
