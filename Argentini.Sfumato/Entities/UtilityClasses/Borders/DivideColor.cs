29a637e645f797adbbeb41fd5e3567ff4c208b47
Michael Argentini <michael@argentini.us>
2025-05-01 16:37:21 -0400

    Added DivideColor, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/Borders/DivideColor.cs
@@ -0,0 +1,54 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Borders;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public sealed class DivideColor : ClassDictionaryBase
+{
+    public DivideColor()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "divide-", new ClassDefinition
+                {
+                    UsesColor = true,
+                    UsesSlashModifier = true,
+                    Template =
+                        """
+                        & > :not(:last-child) {
+                            border-color: {0};
+                        }
+                        """,
+                }
+            },
+            {
+                "divide-inherit", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        & > :not(:last-child) {
+                            border-color: inherit;
+                        }
+                        """,
+                }
+            },
+            {
+                "divide-current", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        & > :not(:last-child) {
+                            border-color: currentColor;
+                        }
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
