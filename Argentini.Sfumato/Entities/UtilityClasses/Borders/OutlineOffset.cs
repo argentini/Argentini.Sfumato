ffb30fd5a0cfd86b369b2158df4a55249ce32444
Michael Argentini <michael@argentini.us>
2025-05-01 17:14:10 -0400

    Added OutlineOffset, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/Borders/OutlineOffset.cs
@@ -0,0 +1,47 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Borders;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public sealed class OutlineOffset : ClassDictionaryBase
+{
+    public OutlineOffset()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "outline-offset-", new ClassDefinition
+                {
+                    UsesInteger = true,
+                    UsesDimensionLength = true,
+                    Template = 
+                        """
+                        outline-offset: {0}px;
+                        """,
+                    ArbitraryCssValueTemplate = 
+                        """
+                        outline-offset: {0};
+                        """,
+                }
+            },
+            {
+                "-outline-offset-", new ClassDefinition
+                {
+                    UsesInteger = true,
+                    UsesDimensionLength = true,
+                    Template = 
+                        """
+                        outline-offset: calc({0}px * -1);
+                        """,
+                    ArbitraryCssValueTemplate = 
+                        """
+                        outline-offset: calc({0} * -1);
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
