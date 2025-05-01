23b7331ea522670d873b432f9387757f00a7ff50
Michael Argentini <michael@argentini.us>
2025-04-30 22:01:43 -0400

    Added MinWidth, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Sizing/MinWidthTests.cs
@@ -0,0 +1,162 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Sizing;
+
+public class MinWidthTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void MinWidth()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "min-w-auto",
+                EscapedClassName = ".min-w-auto",
+                Styles =
+                    """
+                    min-width: auto;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "min-w-px",
+                EscapedClassName = ".min-w-px",
+                Styles =
+                    """
+                    min-width: 1px;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "min-w-fit",
+                EscapedClassName = ".min-w-fit",
+                Styles =
+                    """
+                    min-width: fit-content;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "min-w-xl",
+                EscapedClassName = ".min-w-xl",
+                Styles =
+                    """
+                    min-width: var(--container-xl);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--container-xl" ],
+            },
+            new ()
+            {
+                ClassName = "min-w-3/4",
+                EscapedClassName = @".min-w-3\/4",
+                Styles =
+                    """
+                    min-width: 75%;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing" ],
+            },
+            new ()
+            {
+                ClassName = "min-w-5",
+                EscapedClassName = ".min-w-5",
+                Styles =
+                    """
+                    min-width: calc(var(--spacing) * 5);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing" ],
+            },
+            new ()
+            {
+                ClassName = "min-w-[1.25rem]",
+                EscapedClassName = @".min-w-\[1\.25rem\]",
+                Styles =
+                    """
+                    min-width: 1.25rem;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing" ],
+            },
+            new ()
+            {
+                ClassName = "min-w-(--my-width)",
+                EscapedClassName = @".min-w-\(--my-width\)",
+                Styles =
+                    """
+                    min-width: var(--my-width);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing" ],
+            },
+            new ()
+            {
+                ClassName = "min-w-(length:--my-width)",
+                EscapedClassName = @".min-w-\(length\:--my-width\)",
+                Styles =
+                    """
+                    min-width: var(--my-width);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing" ],
+            },
+            new ()
+            {
+                ClassName = "min-w-[var(--my-width)]",
+                EscapedClassName = @".min-w-\[var\(--my-width\)\]",
+                Styles =
+                    """
+                    min-width: var(--my-width);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing" ],
+            },
+            new ()
+            {
+                ClassName = "min-w-[length:var(--my-width)]",
+                EscapedClassName = @".min-w-\[length\:var\(--my-width\)\]",
+                Styles =
+                    """
+                    min-width: var(--my-width);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+                UsedCssCustomProperties = [ "--spacing" ],
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
