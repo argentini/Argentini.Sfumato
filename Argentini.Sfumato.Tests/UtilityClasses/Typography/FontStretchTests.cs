05f220990f22c3456208e6f3e4586304bd3387c4
Michael Argentini <michael@argentini.us>
2025-04-30 22:50:28 -0400

    Added FontStretch, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Typography/FontStretchTests.cs
@@ -0,0 +1,110 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Typography;
+
+public class FontStretchTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void FontStretch()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "font-stretch-ultra-condensed",
+                EscapedClassName = ".font-stretch-ultra-condensed",
+                Styles =
+                    """
+                    font-stretch: ultra-condensed;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "font-stretch-50%",
+                EscapedClassName = @".font-stretch-50\%",
+                Styles =
+                    """
+                    font-stretch: 50%;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "font-stretch-[66.66%]",
+                EscapedClassName = @".font-stretch-\[66\.66\%\]",
+                Styles =
+                    """
+                    font-stretch: 66.66%;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "font-stretch-[var(--my-pct)]",
+                EscapedClassName = @".font-stretch-\[var\(--my-pct\)\]",
+                Styles =
+                    """
+                    font-stretch: var(--my-pct);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "font-stretch-[percentage:var(--my-pct)]",
+                EscapedClassName = @".font-stretch-\[percentage\:var\(--my-pct\)\]",
+                Styles =
+                    """
+                    font-stretch: var(--my-pct);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "font-stretch-(--my-pct)",
+                EscapedClassName = @".font-stretch-\(--my-pct\)",
+                Styles =
+                    """
+                    font-stretch: var(--my-pct);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "font-stretch-(percentage:--my-pct)",
+                EscapedClassName = @".font-stretch-\(percentage\:--my-pct\)",
+                Styles =
+                    """
+                    font-stretch: var(--my-pct);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.UsedCssCustomProperties.Length, cssClass.UsesCssCustomProperties.Count);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            for (var i = 0; i < test.UsedCssCustomProperties.Length; i++)
+            {
+                Assert.Equal(test.UsedCssCustomProperties.ElementAt(i), cssClass.UsesCssCustomProperties.ElementAt(i));
+            }
+            
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
---
+++ Argentini.Sfumato/Entities/UtilityClasses/Typography/FontStretch.cs
@@ -0,0 +1,111 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Typography;
+
+public sealed class FontStretch : ClassDictionaryBase
+{
+    public FontStretch()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "font-stretch-", new ClassDefinition
+                {
+                    UsesPercentage = true,
+                    UsesAbstractValue = true,
+                    Template = """
+                               font-stretch: {0};
+                               """,
+                    ArbitraryCssValueTemplate =
+                        """
+                        font-stretch: {0};
+                        """
+                }
+            },
+            {
+                "font-stretch-ultra-condensed", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               font-stretch: ultra-condensed;
+                               """
+                }
+            },
+            {
+                "font-stretch-extra-condensed", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               font-stretch: extra-condensed;
+                               """
+                }
+            },
+            {
+                "font-stretch-condensed", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               font-stretch: condensed;
+                               """
+                }
+            },
+            {
+                "font-stretch-semi-condensed", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               font-stretch: semi-condensed;
+                               """
+                }
+            },
+            {
+                "font-stretch-normal", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               font-stretch: normal;
+                               """
+                }
+            },
+            {
+                "font-stretch-semi-expanded", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               font-stretch: semi-expanded;
+                               """
+                }
+            },
+            {
+                "font-stretch-expanded", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               font-stretch: expanded;
+                               """
+                }
+            },
+            {
+                "font-stretch-extra-expanded", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               font-stretch: extra-expanded;
+                               """
+                }
+            },
+            {
+                "font-stretch-ultra-expanded", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               font-stretch: ultra-expanded;
+                               """
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
