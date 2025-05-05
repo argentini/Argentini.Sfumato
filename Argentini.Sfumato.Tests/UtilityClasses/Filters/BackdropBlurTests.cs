48c8f984fe3609cd000360e674f0e92b0d0e7bac
Michael Argentini <michael@argentini.us>
2025-05-05 18:52:01 -0400

    Added BackdropBlur, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Filters/BackdropBlurTests.cs
@@ -0,0 +1,79 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Filters;
+
+public class BackdropBlurTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void BackdropBlur()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "backdrop-blur-none",
+                EscapedClassName = ".backdrop-blur-none",
+                Styles =
+                    """
+                    --sf-backdrop-blur: ;
+                    -webkit-backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "backdrop-blur-[0.5rem]",
+                EscapedClassName = @".backdrop-blur-\[0\.5rem\]",
+                Styles =
+                    """
+                    --sf-backdrop-blur: blur(0.5rem);
+                    -webkit-backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "backdrop-blur-xs",
+                EscapedClassName = ".backdrop-blur-xs",
+                Styles =
+                    """
+                    --sf-backdrop-blur: blur(var(--blur-xs));
+                    -webkit-backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "backdrop-blur-3xl",
+                EscapedClassName = ".backdrop-blur-3xl",
+                Styles =
+                    """
+                    --sf-backdrop-blur: blur(var(--blur-3xl));
+                    -webkit-backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
