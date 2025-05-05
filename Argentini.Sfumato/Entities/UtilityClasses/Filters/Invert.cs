f178c7c5425fb621c9f49cca364e99b6b9097c4d
Michael Argentini <michael@argentini.us>
2025-05-05 18:31:34 -0400

    Added Invert, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/Filters/Invert.cs
@@ -0,0 +1,39 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Filters;
+
+public sealed class Invert : ClassDictionaryBase
+{
+    public Invert()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "invert-", new ClassDefinition
+                {
+                    UsesInteger = true,
+                    UsesAlphaNumber = true,
+                    Template =
+                        """
+                        --sf-invert: invert({0}%);
+                        filter: var(--sf-blur, ) var(--sf-brightness, ) var(--sf-contrast, ) var(--sf-grayscale, ) var(--sf-hue-rotate, ) var(--sf-invert, ) var(--sf-saturate, ) var(--sf-sepia, ) var(--sf-drop-shadow, );
+                        """,
+                }
+            },
+            {
+                "invert", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        --sf-invert: invert(100%);
+                        filter: var(--sf-blur, ) var(--sf-brightness, ) var(--sf-contrast, ) var(--sf-grayscale, ) var(--sf-hue-rotate, ) var(--sf-invert, ) var(--sf-saturate, ) var(--sf-sepia, ) var(--sf-drop-shadow, );
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
