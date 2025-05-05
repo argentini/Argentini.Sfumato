d2566d7e7632e5f8a903d4341f9c9d9678446a33
Michael Argentini <michael@argentini.us>
2025-05-05 19:04:38 -0400

    Added BackdropInvert, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Filters/BackdropInvertTests.cs
@@ -0,0 +1,92 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Filters;
+
+public class BackdropInvertTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void BackdropInvert()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "backdrop-invert",
+                EscapedClassName = ".backdrop-invert",
+                Styles =
+                    """
+                    --sf-backdrop-invert: invert(100%);
+                    -webkit-backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "backdrop-invert-37",
+                EscapedClassName = ".backdrop-invert-37",
+                Styles =
+                    """
+                    --sf-backdrop-invert: invert(37%);
+                    -webkit-backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "backdrop-invert-[.5]",
+                EscapedClassName = @".backdrop-invert-\[\.5\]",
+                Styles =
+                    """
+                    --sf-backdrop-invert: invert(.5);
+                    -webkit-backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "backdrop-invert-(--my-gray)",
+                EscapedClassName = @".backdrop-invert-\(--my-gray\)",
+                Styles =
+                    """
+                    --sf-backdrop-invert: invert(var(--my-gray));
+                    -webkit-backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "backdrop-invert-[var(--my-gray)]",
+                EscapedClassName = @".backdrop-invert-\[var\(--my-gray\)\]",
+                Styles =
+                    """
+                    --sf-backdrop-invert: invert(var(--my-gray));
+                    -webkit-backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    backdrop-filter: var(--sf-backdrop-blur, ) var(--sf-backdrop-brightness, ) var(--sf-backdrop-contrast, ) var(--sf-backdrop-grayscale, ) var(--sf-backdrop-hue-rotate, ) var(--sf-backdrop-invert, ) var(--sf-backdrop-opacity, ) var(--sf-backdrop-saturate, ) var(--sf-backdrop-sepia, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
