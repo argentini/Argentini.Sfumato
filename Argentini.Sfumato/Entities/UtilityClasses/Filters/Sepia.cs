429ff951ff48955dfce1921f6f2a71b5a901c42b
Michael Argentini <michael@argentini.us>
2025-05-05 18:41:55 -0400

    Added Sepia, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/Filters/Sepia.cs
@@ -0,0 +1,44 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Filters;
+
+public sealed class Sepia : ClassDictionaryBase
+{
+    public Sepia()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "sepia-", new ClassDefinition
+                {
+                    UsesInteger = true,
+                    UsesAlphaNumber = true,
+                    Template =
+                        """
+                        --sf-sepia: sepia({0}%);
+                        filter: var(--sf-blur, ) var(--sf-brightness, ) var(--sf-contrast, ) var(--sf-grayscale, ) var(--sf-hue-rotate, ) var(--sf-invert, ) var(--sf-saturate, ) var(--sf-sepia, ) var(--sf-drop-shadow, );
+                        """,
+                    ArbitraryCssValueTemplate = 
+                        """
+                        --sf-sepia: sepia({0});
+                        filter: var(--sf-blur, ) var(--sf-brightness, ) var(--sf-contrast, ) var(--sf-grayscale, ) var(--sf-hue-rotate, ) var(--sf-invert, ) var(--sf-saturate, ) var(--sf-sepia, ) var(--sf-drop-shadow, );
+                        """,
+                }
+            },
+            {
+                "sepia", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        --sf-sepia: sepia(100%);
+                        filter: var(--sf-blur, ) var(--sf-brightness, ) var(--sf-contrast, ) var(--sf-grayscale, ) var(--sf-hue-rotate, ) var(--sf-invert, ) var(--sf-saturate, ) var(--sf-sepia, ) var(--sf-drop-shadow, );
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
