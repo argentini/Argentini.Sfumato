8de4d99118da3dd844fe8e9f1afa64d31940c729
Michael Argentini <michael@argentini.us>
2025-05-04 22:11:20 -0400

    Added Brightness, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Filters/BrightnessTests.cs
@@ -0,0 +1,75 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Filters;
+
+public class BrightnessTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void Brightness()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "brightness-50",
+                EscapedClassName = ".brightness-50",
+                Styles =
+                    """
+                    --sf-brightness: brightness(50%);
+                    filter: var(--sf-blur, ) var(--sf-brightness, ) var(--sf-contrast, ) var(--sf-grayscale, ) var(--sf-hue-rotate, ) var(--sf-invert, ) var(--sf-saturate, ) var(--sf-sepia, ) var(--sf-drop-shadow, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "brightness-150",
+                EscapedClassName = ".brightness-150",
+                Styles =
+                    """
+                    --sf-brightness: brightness(150%);
+                    filter: var(--sf-blur, ) var(--sf-brightness, ) var(--sf-contrast, ) var(--sf-grayscale, ) var(--sf-hue-rotate, ) var(--sf-invert, ) var(--sf-saturate, ) var(--sf-sepia, ) var(--sf-drop-shadow, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "brightness-[250%]",
+                EscapedClassName = @".brightness-\[250\%\]",
+                Styles =
+                    """
+                    --sf-brightness: brightness(250%);
+                    filter: var(--sf-blur, ) var(--sf-brightness, ) var(--sf-contrast, ) var(--sf-grayscale, ) var(--sf-hue-rotate, ) var(--sf-invert, ) var(--sf-saturate, ) var(--sf-sepia, ) var(--sf-drop-shadow, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "brightness-(--my-filter)",
+                EscapedClassName = @".brightness-\(--my-filter\)",
+                Styles =
+                    """
+                    --sf-brightness: brightness(var(--my-filter));
+                    filter: var(--sf-blur, ) var(--sf-brightness, ) var(--sf-contrast, ) var(--sf-grayscale, ) var(--sf-hue-rotate, ) var(--sf-invert, ) var(--sf-saturate, ) var(--sf-sepia, ) var(--sf-drop-shadow, );
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
