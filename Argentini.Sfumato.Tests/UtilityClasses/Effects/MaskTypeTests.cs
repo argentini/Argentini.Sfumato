ea93397d26644f5cafa201e1bdf36ea3defb080a
Michael Argentini <michael@argentini.us>
2025-05-04 21:37:03 -0400

    Added MaskType, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Effects/MaskTypeTests.cs
@@ -0,0 +1,49 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Effects;
+
+public class MaskTypeTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void MaskType()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "mask-type-alpha",
+                EscapedClassName = ".mask-type-alpha",
+                Styles =
+                    """
+                    mask-type: alpha;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "mask-type-luminance",
+                EscapedClassName = ".mask-type-luminance",
+                Styles =
+                    """
+                    mask-type: luminance;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
---
+++ Argentini.Sfumato/Entities/UtilityClasses/Effects/MaskType.cs
@@ -0,0 +1,37 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Effects;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public sealed class MaskType : ClassDictionaryBase
+{
+    public MaskType()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "mask-type-alpha", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        mask-type: alpha;
+                        """,
+                }
+            },
+            {
+                "mask-type-luminance", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        mask-type: luminance;
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
