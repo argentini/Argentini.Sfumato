1c2dbdd156db2be9a981e29ad6511c46f04880ba
Michael Argentini <michael@argentini.us>
2025-05-06 15:10:20 -0400

    Added ForcedColorAdjust, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Accessibility/ForcedColorAdjustTests.cs
@@ -0,0 +1,49 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Accessibility;
+
+public class ForcedColorAdjustTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void ForcedColorAdjust()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "forced-color-adjust-auto",
+                EscapedClassName = ".forced-color-adjust-auto",
+                Styles =
+                    """
+                    forced-color-adjust: auto;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "forced-color-adjust-none",
+                EscapedClassName = ".forced-color-adjust-none",
+                Styles =
+                    """
+                    forced-color-adjust: none;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
---
+++ Argentini.Sfumato/Entities/UtilityClasses/Accessibility/ForcedColorAdjust.cs
@@ -0,0 +1,36 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Accessibility;
+
+public sealed class ForcedColorAdjust : ClassDictionaryBase
+{
+    public ForcedColorAdjust()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "forced-color-adjust-auto", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        forced-color-adjust: auto;
+                        """,
+                }
+            },
+            {
+                "forced-color-adjust-none", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        forced-color-adjust: none;
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
