a3e747b98c6ad7b4586fe37becc20103642704f0
Michael Argentini <michael@argentini.us>
2025-05-06 13:09:12 -0400

    Added FieldSizing, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Interactivity/FieldSizingTests.cs
@@ -0,0 +1,49 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Interactivity;
+
+public class FieldSizingTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void FieldSizing()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "field-sizing-fixed",
+                EscapedClassName = ".field-sizing-fixed",
+                Styles =
+                    """
+                    field-sizing: fixed;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "field-sizing-content",
+                EscapedClassName = ".field-sizing-content",
+                Styles =
+                    """
+                    field-sizing: content;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
