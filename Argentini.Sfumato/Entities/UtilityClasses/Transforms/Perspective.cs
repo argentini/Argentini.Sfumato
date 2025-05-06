2f6f42e8e79e474d4d005f1f8cfe3d0fa224f6bd
Michael Argentini <michael@argentini.us>
2025-05-05 23:15:22 -0400

    Added Perspective, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/Transforms/Perspective.cs
@@ -0,0 +1,54 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Transforms;
+
+public sealed class Perspective : ClassDictionaryBase
+{
+    public Perspective()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "perspective-", new ClassDefinition
+                {
+                    UsesDimensionLength = true,
+                    Template =
+                        """
+                        perspective: {0};
+                        """,
+                }
+            },
+            {
+                "perspective-none", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        perspective: none;
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {
+        foreach (var item in appRunner.AppRunnerSettings.SfumatoBlockItems.Where(i => i.Key.StartsWith("--perspective-")))
+        {
+            var key = item.Key.Trim('-');
+            var value = new ClassDefinition
+            {
+                IsSimpleUtility = true,
+                Template = 
+                    $"""
+                     perspective: var({item.Key});
+                     """,
+            };
+
+            if (appRunner.Library.SimpleClasses.TryAdd(key, value))
+                appRunner.Library.ScannerClassNamePrefixes.Insert(key);
+            else
+                appRunner.Library.SimpleClasses[key] = value;
+        }
+    }
+}
