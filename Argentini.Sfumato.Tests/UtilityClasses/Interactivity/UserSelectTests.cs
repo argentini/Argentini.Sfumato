e732bee2236961ab9a4b9874ac0cc69fd6f22491
Michael Argentini <michael@argentini.us>
2025-05-06 14:40:50 -0400

    Added UserSelect, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Interactivity/UserSelectTests.cs
@@ -0,0 +1,71 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Interactivity;
+
+public class UserSelectTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void UserSelect()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "select-none",
+                EscapedClassName = ".select-none",
+                Styles =
+                    """
+                    user-select: none;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "select-text",
+                EscapedClassName = ".select-text",
+                Styles =
+                    """
+                    user-select: text;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "select-all",
+                EscapedClassName = ".select-all",
+                Styles =
+                    """
+                    user-select: all;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "select-auto",
+                EscapedClassName = ".select-auto",
+                Styles =
+                    """
+                    user-select: auto;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
