4c6b3572298e79ac08a3a82847b2008ac50bcf87
Michael Argentini <michael@argentini.us>
2025-05-05 21:16:54 -0400

    Added TransitionDuration, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/TransitionsAnimations/TransitionDuration.cs
@@ -0,0 +1,45 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.TransitionsAnimations;
+
+public sealed class TransitionDuration : ClassDictionaryBase
+{
+    public TransitionDuration()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "duration-", new ClassDefinition
+                {
+                    UsesInteger = true,
+                    UsesDurationTime = true,
+                    UsesAbstractValue = true,
+                    Template =
+                        """
+                        --sf-duration: {0}ms;
+                        transition-duration: {0}ms;
+                        """,
+                    ArbitraryCssValueTemplate = 
+                        """
+                        --sf-duration: {0};
+                        transition-duration: {0};
+                        """,
+                }
+            },
+            {
+                "duration-initial", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        --sf-duration: initial;
+                        transition-duration: initial;
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
