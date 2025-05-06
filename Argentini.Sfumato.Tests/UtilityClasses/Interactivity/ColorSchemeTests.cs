8d78a417f654bc3eda354c0b1a9c08ecaee07af8
Michael Argentini <michael@argentini.us>
2025-05-06 12:58:11 -0400

    Added ColorScheme, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Interactivity/ColorSchemeTests.cs
@@ -0,0 +1,49 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Interactivity;
+
+public class ColorSchemeTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void ColorScheme()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "scheme-normal",
+                EscapedClassName = ".scheme-normal",
+                Styles =
+                    """
+                    color-scheme: normal;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "scheme-only-light",
+                EscapedClassName = ".scheme-only-light",
+                Styles =
+                    """
+                    color-scheme: only light;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
