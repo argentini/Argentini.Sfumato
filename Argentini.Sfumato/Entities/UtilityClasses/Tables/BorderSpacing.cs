316eb43e5337d0f9e08be9980d534c574f9a6003
Michael Argentini <michael@argentini.us>
2025-05-05 20:39:21 -0400

    Added BorderSpacing, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/Tables/BorderSpacing.cs
@@ -0,0 +1,69 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Tables;
+
+public sealed class BorderSpacing : ClassDictionaryBase
+{
+    public BorderSpacing()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "border-spacing-", new ClassDefinition
+                {
+                    UsesDimensionLength = true,
+                    UsesSpacing = true,
+                    Template =
+                        """
+                        --sf-border-spacing-x: calc(var(--spacing) * {0});
+                        --sf-border-spacing-y: calc(var(--spacing) * {0});
+                        border-spacing: var(--sf-border-spacing-x) var(--sf-border-spacing-y)
+                        """,
+                    ArbitraryCssValueTemplate = 
+                        """
+                        --sf-border-spacing-x: {0};
+                        --sf-border-spacing-y: {0};
+                        border-spacing: var(--sf-border-spacing-x) var(--sf-border-spacing-y)
+                        """,
+                }
+            },
+            {
+                "border-spacing-x-", new ClassDefinition
+                {
+                    UsesDimensionLength = true,
+                    UsesSpacing = true,
+                    Template =
+                        """
+                        --sf-border-spacing-x: calc(var(--spacing) * {0});
+                        border-spacing: var(--sf-border-spacing-x) var(--sf-border-spacing-y)
+                        """,
+                    ArbitraryCssValueTemplate = 
+                        """
+                        --sf-border-spacing-x: {0};
+                        border-spacing: var(--sf-border-spacing-x) var(--sf-border-spacing-y)
+                        """,
+                }
+            },
+            {
+                "border-spacing-y-", new ClassDefinition
+                {
+                    UsesDimensionLength = true,
+                    UsesSpacing = true,
+                    Template =
+                        """
+                        --sf-border-spacing-y: calc(var(--spacing) * {0});
+                        border-spacing: var(--sf-border-spacing-x) var(--sf-border-spacing-y)
+                        """,
+                    ArbitraryCssValueTemplate = 
+                        """
+                        --sf-border-spacing-y: {0};
+                        border-spacing: var(--sf-border-spacing-x) var(--sf-border-spacing-y)
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
