abd6cd88e4319747dbd85c23aa1c99b15cccc1c3
Michael Argentini <michael@argentini.us>
2025-05-06 12:19:31 -0400

    Added TransformOrigin, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/Transforms/TransformOrigin.cs
@@ -0,0 +1,116 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Transforms;
+
+public sealed class TransformOrigin : ClassDictionaryBase
+{
+    public TransformOrigin()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "origin-", new ClassDefinition
+                {
+                    UsesAbstractValue = true,
+                    Template =
+                        """
+                        transform-origin: {0};
+                        """,
+                }
+            },
+            {
+                "origin-center", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        transform-origin: center;
+                        """,
+                }
+            },
+            {
+                "origin-top", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        transform-origin: top;
+                        """,
+                }
+            },
+            {
+                "origin-top-right", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        transform-origin: top right;
+                        """,
+                }
+            },
+            {
+                "origin-right", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        transform-origin: right;
+                        """,
+                }
+            },
+            {
+                "origin-bottom-right", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        transform-origin: bottom right;
+                        """,
+                }
+            },
+            {
+                "origin-bottom", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        transform-origin: bottom;
+                        """,
+                }
+            },
+            {
+                "origin-bottom-left", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        transform-origin: bottom left;
+                        """,
+                }
+            },
+            {
+                "origin-left", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        transform-origin: left;
+                        """,
+                }
+            },
+            {
+                "origin-top-left", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        transform-origin: top left;
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
