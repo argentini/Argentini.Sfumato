8edbe18ad3abdd6e4aae8d0ec13f576a568b86e8
Michael Argentini <michael@argentini.us>
2025-05-06 15:01:11 -0400

    Added Stroke, test

---
+++ Argentini.Sfumato/Entities/UtilityClasses/Svg/Stroke.cs
@@ -0,0 +1,43 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Svg;
+
+public sealed class Stroke : ClassDictionaryBase
+{
+    public Stroke()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "stroke-", new ClassDefinition
+                {
+                    UsesColor = true,
+                    Template = """
+                               stroke: {0};
+                               """,
+                }
+            },
+            {
+                "stroke-inherit", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               stroke: inherit;
+                               """,
+                }
+            },
+            {
+                "stroke-current", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template = """
+                               stroke: currentColor;
+                               """,
+                }
+            },
+        });
+    }
+    
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
