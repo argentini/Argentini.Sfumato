84733c16d5512ae062999b19aad461b29867a761
Michael Argentini <michael@argentini.us>
2025-05-08 17:09:35 -0400

    Added TextUnderlineOffset, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Typography/TextUnderlineOffsetTests.cs
@@ -0,0 +1,93 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Typography;
+
+public class TextUnderlineOffsetTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void TextUnderlineOffset()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "underline-offset-4",
+                EscapedClassName = ".underline-offset-4",
+                Styles =
+                    """
+                    text-underline-offset: 4px;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "underline-offset-4.5",
+                EscapedClassName = @".underline-offset-4\.5",
+                Styles =
+                    """
+                    text-underline-offset: 4.5px;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "-underline-offset-4",
+                EscapedClassName = ".-underline-offset-4",
+                Styles =
+                    """
+                    text-underline-offset: calc(4px * -1);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "-underline-offset-4.5",
+                EscapedClassName = @".-underline-offset-4\.5",
+                Styles =
+                    """
+                    text-underline-offset: calc(4.5px * -1);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "underline-offset-[4.5rem]",
+                EscapedClassName = @".underline-offset-\[4\.5rem\]",
+                Styles =
+                    """
+                    text-underline-offset: 4.5rem;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "underline-offset-(--my-offset)",
+                EscapedClassName = @".underline-offset-\(--my-offset\)",
+                Styles =
+                    """
+                    text-underline-offset: var(--my-offset);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
---
+++ Argentini.Sfumato/Entities/UtilityClasses/Typography/TextUnderlineOffset.cs
@@ -0,0 +1,58 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Typography;
+
+public sealed class TextUnderlineOffset : ClassDictionaryBase
+{
+    public TextUnderlineOffset()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "underline-offset-", new ClassDefinition
+                {
+                    UsesSpacing = true,
+                    UsesAlphaNumber = true,
+                    UsesDimensionLength = true,
+                    Template =
+                        """
+                        text-underline-offset: {0}px;
+                        """,
+                    ArbitraryCssValueTemplate =
+                        """
+                        text-underline-offset: {0};
+                        """,
+                }
+            },
+            {
+                "-underline-offset-", new ClassDefinition
+                {
+                    UsesSpacing = true,
+                    UsesAlphaNumber = true,
+                    UsesDimensionLength = true,
+                    Template =
+                        """
+                        text-underline-offset: calc({0}px * -1);
+                        """,
+                    ArbitraryCssValueTemplate =
+                        """
+                        text-underline-offset: calc({0} * -1);
+                        """,
+                }
+            },
+            {
+                "underline-offset-auto", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        text-underline-offset: auto;
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
