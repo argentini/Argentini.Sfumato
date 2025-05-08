956b84bfae08e3c1b899c305e13088fcb92be6aa
Michael Argentini <michael@argentini.us>
2025-05-08 17:19:59 -0400

    Added TextIndent, test

---
+++ Argentini.Sfumato.Tests/UtilityClasses/Typography/TextIndentTests.cs
@@ -0,0 +1,93 @@
+namespace Argentini.Sfumato.Tests.UtilityClasses.Typography;
+
+public class TextIndentTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void TextIndent()
+    {
+        var appRunner = new AppRunner(new AppState());
+        
+        var testClasses = new List<TestClass>()
+        {
+            new ()
+            {
+                ClassName = "indent-4",
+                EscapedClassName = ".indent-4",
+                Styles =
+                    """
+                    text-indent: calc(var(--spacing) * 4);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "indent-4.5",
+                EscapedClassName = @".indent-4\.5",
+                Styles =
+                    """
+                    text-indent: calc(var(--spacing) * 4.5);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "-indent-4",
+                EscapedClassName = ".-indent-4",
+                Styles =
+                    """
+                    text-indent: calc(var(--spacing) * -4);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "-indent-4.5",
+                EscapedClassName = @".-indent-4\.5",
+                Styles =
+                    """
+                    text-indent: calc(var(--spacing) * -4.5);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "indent-[4.5rem]",
+                EscapedClassName = @".indent-\[4\.5rem\]",
+                Styles =
+                    """
+                    text-indent: 4.5rem;
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+            new ()
+            {
+                ClassName = "indent-(--my-offset)",
+                EscapedClassName = @".indent-\(--my-offset\)",
+                Styles =
+                    """
+                    text-indent: var(--my-offset);
+                    """,
+                IsValid = true,
+                IsImportant = false,
+            },
+        };
+
+        foreach (var test in testClasses)
+        {
+            var cssClass = new CssClass(appRunner, test.ClassName);
+
+            Assert.NotNull(cssClass);
+            Assert.Equal(test.IsValid, cssClass.IsValid);
+            Assert.Equal(test.IsImportant, cssClass.IsImportant);
+            Assert.Equal(test.EscapedClassName, cssClass.EscapedSelector);
+            Assert.Equal(test.Styles, cssClass.Styles);
+
+            testOutputHelper.WriteLine($"{GetType().Name} => {test.ClassName}");
+        }
+    }
+}
---
+++ Argentini.Sfumato/Entities/UtilityClasses/Typography/TextIndent.cs
@@ -0,0 +1,68 @@
+// ReSharper disable RawStringCanBeSimplified
+
+namespace Argentini.Sfumato.Entities.UtilityClasses.Typography;
+
+public sealed class TextIndent : ClassDictionaryBase
+{
+    public TextIndent()
+    {
+        Data.AddRange(new Dictionary<string, ClassDefinition>(StringComparer.Ordinal)
+        {
+            {
+                "indent-", new ClassDefinition
+                {
+                    UsesSpacing = true,
+                    UsesAlphaNumber = true,
+                    UsesDimensionLength = true,
+                    Template =
+                        """
+                        text-indent: calc(var(--spacing) * {0});
+                        """,
+                    ArbitraryCssValueTemplate =
+                        """
+                        text-indent: {0};
+                        """,
+                }
+            },
+            {
+                "-indent-", new ClassDefinition
+                {
+                    UsesSpacing = true,
+                    UsesAlphaNumber = true,
+                    UsesDimensionLength = true,
+                    Template =
+                        """
+                        text-indent: calc(var(--spacing) * -{0});
+                        """,
+                    ArbitraryCssValueTemplate =
+                        """
+                        text-indent: calc({0} * -1);
+                        """,
+                }
+            },
+            {
+                "indent-px", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        text-indent: 1px;
+                        """,
+                }
+            },
+            {
+                "-indent-px", new ClassDefinition
+                {
+                    IsSimpleUtility = true,
+                    Template =
+                        """
+                        text-indent: -1px;
+                        """,
+                }
+            },
+        });
+    }
+
+    public override void ProcessThemeSettings(AppRunner appRunner)
+    {}
+}
