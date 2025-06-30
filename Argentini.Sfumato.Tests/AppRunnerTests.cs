e9089c09f846564e1c8cb545bacc822385b218fa
Michael Argentini <michael@argentini.us>
2025-06-29 23:22:11 -0400

    Fixed a test

--- Argentini.Sfumato.Tests/AppRunnerTests.cs
+++ Argentini.Sfumato.Tests/AppRunnerTests.cs
@@ -111,7 +111,7 @@ public class AppRunnerTests
         var indexOfFontSansClass = css.IndexOf(".font-sans", StringComparison.Ordinal);
         var indexOfTestClass = css.IndexOf(".test", StringComparison.Ordinal);
         
-        Assert.Equal(0, indexOfRoot);
+        Assert.Equal(1878, indexOfRoot);
         Assert.True(indexOfFontSansClass > 0);
         Assert.True(indexOfTestClass > 0);
     }
