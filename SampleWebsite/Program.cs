fde1fdc9c97f2dbe3c54b45efd5808ae9f220f40
Michael Argentini <michael@argentini.us>
2025-11-15 21:39:48 -0500

    Using preprocessor directive for sample site watcher

--- SampleWebsite/Program.cs
+++ SampleWebsite/Program.cs
@@ -6,16 +6,16 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    Sfumato.Service.StartWatcher("wwwroot/stylesheets/source.css");
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+#if DEBUG
+    Sfumato.Service.StartWatcher("wwwroot/stylesheets/source.css", minify: false);
+#endif
 
 app.UseHttpsRedirection();
 
