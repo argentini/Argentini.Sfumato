6e4e7740d99845a7e6d7aea2906564341ebf2b77
Michael Argentini <michael@argentini.us>
2025-11-15 21:20:06 -0500

    Updated pack scripts

--- pack.ps1
+++ pack.ps1
@@ -1,11 +1,13 @@
-if (Test-Path ".\Sfumato.Cli\nupkg") { Remove-Item ".\Sfumato.Cli\nupkg" -Recurse -Force }
 . ./clean.ps1
+
+if (Test-Path ".\Sfumato.Cli\nupkg") { Remove-Item ".\Sfumato.Cli\nupkg" -Recurse -Force }
 Set-Location Sfumato.Cli
 dotnet pack --configuration Release
 Set-Location ..
 
 if (Test-Path ".\Sfumato\nupkg") { Remove-Item ".\Sfumato\nupkg" -Recurse -Force }
-. ./clean.ps1
 Set-Location Sfumato
 dotnet pack --configuration Release
 Set-Location ..
+
+. ./clean.ps1
--- pack.sh
+++ pack.sh
@@ -1,11 +1,13 @@
-rm -r Sfumato/nupkg
 source clean.sh
+
+rm -r Sfumato/nupkg
 cd Sfumato
 dotnet pack --configuration Release
 cd ..
 
 rm -r Sfumato.Cli/nupkg
-source clean.sh
 cd Sfumato.Cli
 dotnet pack --configuration Release
 cd ..
+
+source clean.sh
